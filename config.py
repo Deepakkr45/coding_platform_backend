8cf2e9241b7c3caa2b8c5966746080f208250289
aryananuj <aryananuj294@gmail.com>
2025-03-12 12:19:18 +0530

    updated the uri in the config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-backend_api = 'http://cloud.xtranssolutions.com/node/api/channels/{channel_id}/entries/read'
+read_api = 'http://cloud.xtranssolutions.com/node/api/channels/{channel_id}/entries/read'
 
-export_api = 'http://cloud.xtranssolutions.com/node/api/channels/{channel_id}/fields/csv'
+export_api = 'http://cloud.xtranssolutions.com/node/api/csv/channels/{channel_id}/fields/csv'
+
+channel_fetch = 'http://cloud.xtranssolutions.com/node/api/auth/channels'
